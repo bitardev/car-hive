de2942ecbfcb0df6a89c6e5fb8659f68f7caa8ef
Bitar Youssef <bitardev@gmail.com>
2024-09-29 22:43:47 +0100

    Finished the Header, Footer and Hero Section components

---
+++ components/CustomButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  btnType,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {
+        handleClick;
+      }}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
